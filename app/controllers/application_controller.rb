a404f971ac1cb96099d9ca9c2802d9a3f998f0fe
Laguna1 <lagunazoomarket@gmail.com>
2021-11-11 19:42:35 +0200

    Costomize devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-    # before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-    # protected
+    protected
   
-    # def configure_permitted_parameters
-    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :mobile_no, :address])
-    #   devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :mobile_no, :address])
-    # end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :mobile_no, :address])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :mobile_no, :address])
+    end
 end
