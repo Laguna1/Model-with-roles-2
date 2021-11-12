469cdc1cfa2ff001c716b04942e232aa057f5b1e
Laguna1 <lagunazoomarket@gmail.com>
2021-11-12 23:04:41 +0200

    Seed categories content

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem "devise", "~> 4.8"
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -17,7 +19,17 @@
 # u3 = User.create({ fullname: 'Kevin Spacy', mobile_no: '380672222222', address: 'Kharkiv, Danilevskogo str. 9', email: 'kevin@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r2.id })
 # u4 = User.create({ fullname: 'Jack Nortingen', mobile_no: '380733333333', address: 'Kharkiv, Danilevskogo str. 17', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r3.id })
 
-v1 = Vizit.create({ date: '2021-12-10 11:00:00', notes: 'Headache', user_id: 1 })
-v2 = Vizit.create({ date: '2021-11-12 12:00:00', notes: 'Toothache', user_id: 6 })
-v3 = Vizit.create({ date: '2021-11-12 16:30:00', notes: 'Injury of the joints of the hand', user_id: 1 })
-v4 = Vizit.create({ date: '2021-11-15 15:45:00', notes: 'Stomach pain', user_id: 6 })
+# v1 = Vizit.create({ date: '2021-12-10 11:00:00', notes: 'Headache', user_id: 1 })
+# v2 = Vizit.create({ date: '2021-11-12 12:00:00', notes: 'Toothache', user_id: 6 })
+# v3 = Vizit.create({ date: '2021-11-12 16:30:00', notes: 'Injury of the joints of the hand', user_id: 1 })
+# v4 = Vizit.create({ date: '2021-11-15 15:45:00', notes: 'Stomach pain', user_id: 6 })
+
+c1 = Category.create(speciality: 'Therapist')
+c2 = Category.create(speciality: 'Surgeon')
+c3 = Category.create(speciality: 'Pediatrician')
+c4 = Category.create(speciality: 'Gynecologist')
+c5 = Category.create(speciality: 'Dentist')
+c6 = Category.create(speciality: 'Dermatologist')
+c7 = Category.create(speciality: 'Ophthalmologist')
+c8 = Category.create(speciality: 'Urologist')
+c9 = Category.create(speciality: 'Psychiatrist')
