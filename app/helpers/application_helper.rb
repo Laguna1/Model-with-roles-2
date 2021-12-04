7c4348cb411cbb7e7381299984bac3649ec78d60
Laguna1 <lagunazoomarket@gmail.com>
2021-12-05 00:52:16 +0200

    Add book/unbook actiont to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,41 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  # Checks whether or not a user has had a contact request sent to them by the current user
-  # returning either true or false
-  def contact_request_sent?(user)
-    current_user.contact_sent.exists?(doctor_id: user.id, status: false)
+  # # Checks whether or not a user has had a contact request sent to them by the current user
+  # # returning either true or false
+  # def contact_request_sent?(user)
+  #   current_user.contact_sent.exists?(doctor_id: user.id, status: false)
+  # end
+
+  # # Checks whether or not a user has sent a contact request to the current user
+  # # returning either true or false
+  # # Probably it is not necessary, we don`t need requests from the doctor to patient
+  # def contact_request_recieved?(user)
+  #   current_user.contact_request.exists?(patient_id: user.id, status: false)
+  # end
+
+  # # Checks whether or not a user has had a contact request sent to them by the current user
+  # # or if the current user has been sent a contact request by the user
+  # # and the reciever has accepted the contact
+  # # returning either true or false
+  # def contact_request_status?(user)
+  #   request_sent = current_user.contact_sent.exists?(doctor_id: user.id, status: true)
+  #   request_recieved = current_user.contact_request.exists?(patient_id: user.id, status: true)
+
+  #   return true if request_sent != request_recieved
+  #   return true if request_sent == request_recieved && request_sent == true
+  #   return false if request_sent == request_recieved && request_sent == false
+  # end
+
+  def book
+    @user = User.find(params[:id])
+    current_user.doctors << @user
+    redirect_back(fallback_location: user_path(@user))
   end
 
-  # Checks whether or not a user has sent a contact request to the current user
-  # returning either true or false
-  # Probably it is not necessary, we don`t need requests from the doctor to patient
-  def contact_request_recieved?(user)
-    current_user.contact_request.exists?(patient_id: user.id, status: false)
-  end
-
-  # Checks whether or not a user has had a contact request sent to them by the current user
-  # or if the current user has been sent a contact request by the user
-  # and the reciever has accepted the contact
-  # returning either true or false
-  def contact_request_status?(user)
-    request_sent = current_user.contact_sent.exists?(doctor_id: user.id, status: true)
-    request_recieved = current_user.contact_request.exists?(patient_id: user.id, status: true)
-
-    return true if request_sent != request_recieved
-    return true if request_sent == request_recieved && request_sent == true
-    return false if request_sent == request_recieved && request_sent == false
+  def unbook
+    @user = User.find(params[:id])
+    current_user.doctor_relationships.find_by(doctor_id: @user.id).destroy
+    redirect_back(fallback_location: user_path(@user))
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def current_user_is_doctor?(current_user_id, doctor_id)
+    relationship = Contact.find_by(patient_id: current_user_id, doctor_id: doctor_id)
+    return true if relationship
+  end
 end
