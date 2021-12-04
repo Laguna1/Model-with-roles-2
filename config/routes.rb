a77c250a3263cb77d90e6e8c85a5cba8c246f053
Laguna1 <lagunazoomarket@gmail.com>
2021-12-05 00:46:29 +0200

    Add contacts controller, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   resources :vizits
   resources :roles
   resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :contacts
 end
