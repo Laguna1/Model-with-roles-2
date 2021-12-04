5533c73b4dc5e76b76a2167d258f8c0792076b95
Laguna1 <lagunazoomarket@gmail.com>
2021-12-04 23:53:43 +0200

    Destroy Contact model

--- app/models/contact.rb
+++
@@ -1,5 +0,0 @@
-class Contact < ApplicationRecord
-  belongs_to :patient, class_name: 'User', foreign_key: 'patient_id'
-  belongs_to :doctor, class_name: 'User', foreign_key: 'doctor_id'
-  scope :contactors, -> { where('status =?, true') }
-end
