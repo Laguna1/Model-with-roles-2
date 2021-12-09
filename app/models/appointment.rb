86dd87829812d727a1aca8885d72d60515ed2394
Laguna1 <lagunazoomarket@gmail.com>
2021-12-09 04:22:08 +0200

    Add uniqueness visit_id validation for appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,5 +5,6 @@ class Appointment < ApplicationRecord
   belongs_to :pat, class_name: 'User'
   belongs_to :doc, class_name: 'User'
 
+  validates :visit_id, uniqueness: true
   validates :recomendation, presence: true, length: { minimum: 20 }
 end
